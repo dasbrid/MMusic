df906caeb20186ac021989a416ecb2d0a81ebf19
David <stickdave2003@gmail.com>
2015-12-20 20:27:19 +0100

    Fixed playqueuesize setting (but don't enter a non integer string!!!!)

--- src/asbridge/me/uk/MMusic/utils/Settings.java
+++ src/asbridge/me/uk/MMusic/utils/Settings.java
@@ -2,6 +2,7 @@ package asbridge.me.uk.MMusic.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 /**
  * Created by David on 19/12/2015.
@@ -11,6 +12,10 @@ public class Settings {
     public static int getPlayQueueSize(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences("myapp",Context.MODE_PRIVATE);
         int pqs = sharedPref.getInt("playqueuesize", 8);
-        return pqs;
+
+        SharedPreferences defaultSharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String playQueueSize = defaultSharedPref.getString("playqueuesize", "3");
+
+        return Integer.parseInt(playQueueSize);
     }
 }
